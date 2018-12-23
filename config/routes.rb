c750100b530d90762c135bf5b87e1007852c2f8b
Gary Crockett <gbcrockett@gmail.com>
2018-12-23 16:58:44 -0500

    Add controllers for Parsec objects

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  get 'bs_page/home'
+  resources :products
+  resources :projects
+  resources :shipments
+  resources :distributors
+  resources :registrations
+  resources :users
   root 'application#hello'
 end
